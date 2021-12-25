c918f0ac230e4c77005d51ff660a93895b25734e
emrevlog <61878543+emrevlog@users.noreply.github.com>
2021-12-25 21:49:41 +0300

    update 2

--- OpenCV-Python-Series-master/src/dataSetCreator.py
+++ OpenCV-Python-Series-master/src/dataSetCreator.py
@@ -7,58 +7,57 @@ detector = cv2.CascadeClassifier(
     "cascades\data\haarcascade_frontalface_default.xml")
 
 
-def insertOrUpdate(Id, Name):
+def insertOrUpdate(id, name, password, mail):
     conn = sqlite3.connect("FaceBase.db")
-    cmd = "SELECT * FROM People WHERE ID="+str(Id)
+    cmd = "SELECT * FROM People WHERE ID="+str(id)
     cursor = conn.execute(cmd)
     isRecordExist = 0
     for row in cursor:
         isRecordExist = 1
     if(isRecordExist == 1):
-        cmd = "UPDATE people SET Name=' "+str(name)+" ' WHERE ID="+str(Id)
+        cmd = "UPDATE People SET Name=' " + \
+            str(name)+" ', Password='"+str(password) + \
+            "', Email='"+str(mail)+"' WHERE ID="+str(id)
 
     else:
-        cmd = "INSERT INTO people(ID,Name) Values(" + \
-            str(Id)+",' "+str(name)+" ' )"
+        cmd = "INSERT INTO People(Id,Name,Password,Email) Values(" + \
+            str(id)+",' "+str(name)+" ,'"+str(password)+",'"+str(mail)+" )"
 
     conn.execute(cmd)
     conn.commit()
     conn.close()
 
 
-id = input('enter user id')
-name = input('enter your name')
-insertOrUpdate(id, name)
-dirName = "D:\\programlama-dilleri\\FaceRecognition\\OpenCV-Python-Series-master\\src\\images\\" + name
+def createDataSet(id, name):
+    dirName = "D:\\programlama-dilleri\\FaceRecognition\\OpenCV-Python-Series-master\\src\\images\\" + name
 
+    if not os.path.exists(dirName):
+        # exit
+        os.makedirs(dirName, exist_ok=True)
+    sampleNum = 0
 
-if not os.path.exists(dirName):
-    # exit
-    os.makedirs(dirName, exist_ok=True)
-sampleNum = 0
+    while(True):
+        ret, img = cap.read()
 
-while(True):
-    ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = detector.detectMultiScale(gray, 1.5, 5)
+        for (x, y, w, h) in faces:
+            roi_color = img[y:y+h, x:x+w]
+            sampleNum = sampleNum+1
+            # cv2.imwrite("D:\\programlama-dilleri\\Face-recognization-with-SQLite-Database-master\\Face-recognization-with-SQLite-Database-master\\dataSet\\"+str(id)+"." +
+            #             str(sampleNum)+".jpg", gray[y:y+h, x:x+w])
+            cv2.imwrite("D:\\programlama-dilleri\\FaceRecognition\\OpenCV-Python-Series-master\\src\\images\\"+str(name)+"\\"+str(id)+"." +
+                        str(sampleNum)+".jpg", roi_color)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.waitKey(100)
 
-    faces = detector.detectMultiScale(gray, 1.5, 5)
-    for (x, y, w, h) in faces:
-        roi_color = img[y:y+h, x:x+w]
-        sampleNum = sampleNum+1
-        # cv2.imwrite("D:\\programlama-dilleri\\Face-recognization-with-SQLite-Database-master\\Face-recognization-with-SQLite-Database-master\\dataSet\\"+str(id)+"." +
-        #             str(sampleNum)+".jpg", gray[y:y+h, x:x+w])
-        cv2.imwrite("D:\\programlama-dilleri\\FaceRecognition\\OpenCV-Python-Series-master\\src\\images\\"+str(name)+"\\"+str(id)+"." +
-                    str(sampleNum)+".jpg", roi_color)
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        cv2.waitKey(100)
+        cv2.imshow('Face', img)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+        # cv2.waitKey(1)
+        if(sampleNum > 50):
+            break
 
-    cv2.imshow('Face', img)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-    # cv2.waitKey(1)
-    if(sampleNum > 20):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
